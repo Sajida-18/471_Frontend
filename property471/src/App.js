d79cebbd14dda2c19b87fe07ec0b8a1ff190af1b
Sajida-18 <sajida.rifat99@gmail.com>
2023-10-24 22:02:32 +0600

    Navbar

--- property471/src/App.js
+++ property471/src/App.js
@@ -1,12 +1,37 @@
 //import logo from './logo.svg';
 import './App.css';
 import Hello from './Components/Hello';
-
+import Agent from './Components/Agent';
+import Dashboard from './Components/Dashboard';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Property from './Components/Property';
+import SignupLogin from './Components/SignupLogin';
+import Support from './Components/Support';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  //Link
+ } from "react-router-dom";
 function App() {
   return (
     <>
-    <h1>hello react</h1>
     <Hello></Hello>
+    <Router> 
+    <Navbar title="Property471"/> 
+    <div className="container my-3">
+    <Routes>
+      <Route path="/Support" element={<Support/>}/>
+      <Route path="/Dashboard" element={<Dashboard/>}/>
+      <Route path="/SignupLogin" element={<SignupLogin/>}/>
+      <Route path="/Agent" element={<Agent/>}/>
+      <Route path="/Property" element={<Property/>}/>
+      <Route path="/" element={<Home heading="Welcome"/>}>
+      </Route>
+   </Routes>
+    </div>
+   </Router>
 
     </>
   );
---
+++ property471/src/Components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Home_Page-{props.heading}</h1>
+    </div>
+  )
+}
