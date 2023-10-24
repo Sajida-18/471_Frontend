e40d21a736431337e2d387cfe644324f75546e18
Zabir Ramiz <zabir.ramiz@g.bracu.ac.bd>
2023-10-24 18:14:11 +0600

    connected backend with frontend using axios

--- property471/src/App.js
+++ property471/src/App.js
@@ -1,15 +1,44 @@
 //import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Hello from './Components/Hello';
+import axios from 'axios';
 
-function App() {
-  return (
-    <>
-    <h1>hello react</h1>
-    <Hello></Hello>
+class App  extends React.Component {
 
-    </>
-  );
+  state = {details: [],}
+
+  componentDidMount() {
+    let data;
+    axios.get('http://127.0.0.1:8000/hello_world_function/first_function_url')
+
+      .then(res => {
+        data = res.data;
+        this.setState({
+          details: data
+
+        });
+      })
+      .catch(err => { })
+  }
+
+  render() {
+    return(
+      <div>
+        <header>the data that i got</header>
+        <hr />
+        {this.state.details.map((out) => (
+          <div>
+            {out.hello} <br />
+            {out.world} <br />
+            
+            <br /><br />
+          </div>
+        ))}
+      </div>
+    )
+  }
+
 }
 
 export default App;
