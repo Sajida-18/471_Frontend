03fff5fb7abfb0cc5ca5821f9e9850bbd68b631d
Purbae16 <87410863+Purbae16@users.noreply.github.com>
2023-11-17 14:24:27 +0600

    dashboard

--- property471/src/Components/UserComponents/UserDashboard.js
+++ property471/src/Components/UserComponents/UserDashboard.js
@@ -14,8 +14,9 @@
 import React, { useState } from 'react';
 import { Container, Button, Form, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
-const Dashboard = () => {
+const UserDashboard = ({setUserType}) => {
   const [isEditingProfile, setEditingProfile] = useState(false);
   const [isAddingProperty, setAddingProperty] = useState(false);
 
@@ -25,10 +26,11 @@ const Dashboard = () => {
   const [email, setEmail] = useState('');
 
   const [propertyName, setPropertyName] = useState('');
-  const [propertyType, setPropertyType] = useState('');
+  const [propertyPrice, setPropertyPrice] = useState('');
   const [address, setAddress] = useState('');
   const [size, setSize] = useState('');
-  const [image, setImage] = useState('');
+  const [propertyErrorMessage, setPropertyErrorMessage] = useState("");
+  //const [image, setImage] = useState('');
 
   const navigate = useNavigate();
 
@@ -46,14 +48,53 @@ const Dashboard = () => {
   };
 
   const handleSaveProperty = () => {
-    // Perform save logic (e.g., API request to save property details)
-    // After saving, navigate back to the main dashboard
-    setAddingProperty(false);
-  };
-   console.log(isEditingProfile)
+    if (propertyName && propertyPrice && address && size) {
+      const propertyData = {
+        user_id : username,
+        property_name : propertyName,
+        property_price : propertyPrice,
+        property_location : address,
+        property_size : size,
+      };
+
+      axios
+          .post("http://127.0.0.1:8000/api/property/create_property", propertyData, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+          .then((response) => {
+            // setUserType(response.data["data"].type)
+
+            if (response.status === 201) {
+              // Registration was successful
+              setUserType(response.data.data.type);
+              console.log("Registration was successful.");
+
+              console.log(response.data["data"].type);
+              // You can perform actions based on success, such as redirecting to the dashboard
+              setAddingProperty(false);
+            } else {
+              // Handle other HTTP status codes as needed
+              console.error(
+                "Registration failed with status code:",
+                response.status
+              );
+            }
+          })
+          .catch((error) => {
+            // Handle any network or server errors
+            console.error("Network/server error:", error);
+          });
+        } else {
+          setPropertyErrorMessage(
+            <div style={{ color: "red" }}>All fields are required</div>
+          );
+        }
+      };
+
   return (
-     <div>
-    {/* <Container> */}
+    <Container>
       <div className="bg-light p-4 mb-4 rounded-lg">
         <h1 className="text">Dashboard</h1>
         <p>{username}</p>
@@ -67,9 +108,8 @@ const Dashboard = () => {
           </Col>
         </Row>
       </div>
-      {/* </Container> */}
+
       {isEditingProfile && (
-        
         <Form>
           <Form.Group controlId="formName">
             <Form.Label>Name</Form.Label>
@@ -105,7 +145,7 @@ const Dashboard = () => {
       )}
 
       <div className="mt-4">
-        <h2>Your Properties</h2>
+        <h2>My Properties</h2>
         {!isAddingProperty && (
           <Button variant="primary" className="w-100" onClick={handleAddProperty}>
             Add Property
@@ -122,24 +162,27 @@ const Dashboard = () => {
               placeholder="Enter property name"
               value={propertyName}
               onChange={(e) => setPropertyName(e.target.value)}
+              required
             />
           </Form.Group>
-          <Form.Group controlId="formPropertyType">
-            <Form.Label>Property Name</Form.Label>
+          <Form.Group controlId="formPropertyPrice">
+            <Form.Label>Property price</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Enter property type"
-              value={propertyType}
-              onChange={(e) => setPropertyType(e.target.value)}
+              placeholder="Enter property price"
+              value={propertyPrice}
+              onChange={(e) => setPropertyPrice(e.target.value)}
+              required
             />
           </Form.Group>
           <Form.Group controlId="formAddress">
-            <Form.Label>Address</Form.Label>
+            <Form.Label>Location</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Enter address"
+              placeholder="Enter property location"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
+              required
             />
           </Form.Group>
           <Form.Group controlId="formSize">
@@ -149,26 +192,19 @@ const Dashboard = () => {
               placeholder="Enter size"
               value={size}
               onChange={(e) => setSize(e.target.value)}
+              required
             />
           </Form.Group>
-          <Form.Group controlId="formImage">
-            <Form.Label>Image URL</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter image URL"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-            />
-          </Form.Group>
+          {propertyErrorMessage && (
+                  <div className="error-message">{propertyErrorMessage}</div>
+          )}
           <Button variant="success" onClick={handleSaveProperty} className="mt-3">
             Save Property
           </Button>
         </Form>
       )}
-    {/* // </Container> */}
-    </div>
-    
+    </Container>
   );
 };
 
-export default Dashboard;
+export default UserDashboard;
