91c4d875895e8be9e85751e427eeab9f95164cf3
Purbae16 <87410863+Purbae16@users.noreply.github.com>
2023-11-18 09:41:43 +0600

    edit profile/hire

--- property471/src/Components/AdminComponents/EmployeeDetails.js
+++ property471/src/Components/AdminComponents/EmployeeDetails.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -18,7 +17,43 @@ const EmployeeDetails = () => {
 
   return (
     <div>
-      employee details for admin
+      <h1 align="center">Employee Details</h1>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr align="left">
+              <th scope="col">EMPLOYEE_ID</th>
+              <th scope="col">JOB</th>
+              <th scope="col">NAME</th>
+              <th scope="col">EMAIL</th>
+              <th scope="col">ADDRESS</th>
+              <th scope="col">PHONE</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(employeeData) ? (
+              employeeData.map((employee) => (
+                <tr key={employee.employee_id}>
+                  <td>{employee.employee_id}</td>
+                  <td>{employee.type}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.address}</td>
+                  <td>{employee.phone}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6">No employee data available</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default EmployeeDetails;
